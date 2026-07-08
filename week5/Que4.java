fa32a922553daf7407fbb23de5021b2a2f0b18a5
ming694259021-cpu <694259021@webmail.npru.ac.th>
2026-07-09 00:44:15 +0700

    add Que4.java

--- week5/Que4.java
+++ week5/Que4.java
@@ -3,6 +3,6 @@ void main () {
    int deductionper = Integer.parseInt(IO.readln("Enter deduction percent : "));
    double deduction = salary * deductionper / 100;
    double Net = salary * deduction;
-   IO.println("Deduction = %.2f ".formatted(deduction));
-   IO.println("Net salary = %.2f ".formatted(Net));
+   IO.println("Deduction = %.1f ".formatted(deduction));
+   IO.println("Net salary = %.1f ".formatted(Net));
 }
